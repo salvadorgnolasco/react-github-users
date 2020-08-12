8986984619f6b6b3dd41be7e04e3fea6464bdfa3
Salvador González Nolasco <salvador.gonzalez-nolasco@capgemini.com>
2020-08-11 21:12:33 -0500

    simple style for error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,23 +1,21 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import React from 'react';
 
 function Error() {
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="error-template">
-            <h1>Oops!</h1>
-            <h2>404 Not Found</h2>
-            <p>Sorry, an error has occured, Requested page not found!</p>
-            <Link to="/" className="btn btn-primary btn-lg mt-3">
-              Take me Home
+    <main className="container mt-5">
+      <section className="row">
+        <div className="col">
+          <h1>Oops!</h1>
+          <h2>404 Not Found</h2>
+          <p>Sorry, an error has occured, Requested page not found!</p>
+          <Link to="/" className="btn btn-primary btn-md mt-3">
+            Take me Home
             </Link>
-          </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
 
